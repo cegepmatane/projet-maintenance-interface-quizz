752d875573c725e39431370035430557f687cfe0
touggani <35734200+touggani@users.noreply.github.com>
2018-02-19 10:42:25 -0500

    modification du main pour l'affiche

--- Quizz_Refactoring/src/version_2/Main.java
+++ Quizz_Refactoring/src/version_2/Main.java
@@ -9,15 +9,15 @@ public class Main {
 	public static void main(String[] args) {
 		JFrame f1 = new JFrame("Quizz");
 		f1.setSize(1250, 730);
-		f1.setVisible(true);
-		f1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
 		
 		
 		
 		Vue vue = new Vue();
 		Controleur controleur = new Controleur(vue);
-		Quizz modele = new Quizz(controleur);
+	//	Quizz modele = new Quizz(controleur);
 		f1.add(vue,BorderLayout.CENTER);
-		
+		f1.setVisible(true);
+		f1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
 }
